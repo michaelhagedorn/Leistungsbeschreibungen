1f03ab92157aee22120578362035f5b1c626df1d
michaelhagedorn <michael.hagedorn@live.de>
2013-10-23 22:42:29 +0200

    bereinigt

--- index2.php
+++ index2.php
@@ -1,13 +1,5 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-echo "added some php and edited it";
-
 ?>
 
 Index2.php just for testing
