fcc56bf17277a7d1566ed9d0db65da520fe4bdfd
admin <admin@shdewuenbmha>
2013-10-23 23:00:16 +0200

    Merge origin/master
    Conflicts:
    	index2.php

--- classes/c_ajax.php
+++ classes/c_ajax.php
@@ -9,7 +9,7 @@ class c_ajax {
 	function update_db_field_and_return($Tabelle, $Spalte, $Neuer_Wert, $Index, $IndexID) {
 		
 		$sql = "UPDATE $Tabelle SET $Spalte = '$Neuer_Wert' WHERE $Index = '$IndexID' ";
-		//echo $sql;		
+		//echo $SQL;		
 	}
 }
 ?>
