7506fb058c63f83f645f26d90eb4164518cd1514
michaelhagedorn <michael.hagedorn@live.de>
2013-10-23 22:33:01 +0200

    Update includes.php

--- includes.php
+++ includes.php
@@ -1,11 +1,13 @@
 <?php
+date_default_timezone_set('Europe/Berlin'); //timezone
+// DB-Connection
 include('ezSQL/shared/ez_sql_core.php');
 include('ezSQL/mysql/ez_sql_mysql.php');
-date_default_timezone_set('Europe/Berlin'); //timezone
+
+// Klassen
 include('classes/c_task.php');
 include('classes/c_session.php');
 include('classes/c_input.php');
 include('classes/c_ajax.php');
-
 ?>
 
