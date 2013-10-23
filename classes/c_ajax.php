ed8ea1fc6519624dd4145777272e270e973ebf1f
michaelhagedorn <michael.hagedorn@live.de>
2013-10-23 23:10:44 +0200

    bereinigt

--- classes/c_ajax.php
+++ classes/c_ajax.php
@@ -2,20 +2,13 @@
 
 class c_ajax {
     
-	function __construct($ID) {
-		// added $ID
+	function __construct() {
 		$this->update_db_field_and_return('tasks', 'task_time', '120', 'task_id', '19');
 	}
 	
 	function update_db_field_and_return($Tabelle, $Spalte, $Neuer_Wert, $Index, $IndexID) {
 		
 		$sql = "UPDATE $Tabelle SET $Spalte = '$Neuer_Wert' WHERE $Index = '$IndexID' ";
-		// added commant with $ID
 	}
-}
-
-function dig()
-{
-  echo "added";  
 }
 ?>
